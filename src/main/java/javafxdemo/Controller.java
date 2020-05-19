2d9b7fda8176504b410235c0819f5d88afad410a
Hristo Raykov <hristocr@gmail.com>
2020-05-19 22:32:59 +0300

    loading stock lists from file

--- src/main/java/javafxdemo/Controller.java
+++ src/main/java/javafxdemo/Controller.java
@@ -1,4 +1,61 @@
 package javafxdemo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void loadSymbols() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+
+        File file = fileChooser.showOpenDialog(stage);
+
+        List<String> lines = getFileLines(file);
+
+        List<String> symbols = getIBSymbols(lines);
+
+
+
+        System.out.println();
+    }
+
+    private List<String> getIBSymbols(List<String> lines) {
+        List<String> symbols = new ArrayList<>();
+
+        for (int i = 1; i < lines.size(); i++) {
+            String line = lines.get(i);
+            String symbol = line.split(",")[1];
+            symbols.add(symbol);
+        }
+
+        return symbols;
+    }
+
+    private List<String> getFileLines(File file) throws IOException {
+        List<String> lines = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String line;
+        while ((line = reader.readLine()) != null){
+            lines.add(line);
+        }
+
+        return lines;
+    }
+
+
 }
--- src/main/java/javafxdemo/Main.java
+++ src/main/java/javafxdemo/Main.java
@@ -2,16 +2,31 @@ package javafxdemo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("auction_gaps.fxml"));
+        Parent root = loader.load();
+
+        Controller controller = loader.getController();
+        controller.setStage(primaryStage);
+
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setTitle("Auction Gaps");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
